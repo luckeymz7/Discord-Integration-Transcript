3970b51ec581802b8c1fc730c754534f658929db
Artur Schincariol Rossi <artursrossi4@gmail.com>
2023-10-12 01:54:52 -0300

    Update Experimentals imports next.config.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,6 @@
 "use client";
-import axios from "axios";
-
 import styles from "./page.module.css";
-import { useEffect } from "react";
 
 export default async function Home() {
-  // useEffect(async () => {
-  //   await axios.get("/api/fetchHTML");
-  // });
-
-  return <div></div>;
+  return <h1>Discord Integration Transcript </h1>;
 }
