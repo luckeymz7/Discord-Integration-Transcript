ce02c099e8b2fa2c16faee900d3735ce3d980873
Artur Schincariol Rossi <artursrossi4@gmail.com>
2023-10-15 02:04:05 -0300

    Transcripts map

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-
 import styles from "./page.module.css";
 
 const prisma = new PrismaClient();
@@ -21,5 +20,41 @@ export default async function Home({ searchParams }) {
     return <iframe className={styles.iframe} srcDoc={res}></iframe>;
   }
 
-  return <h1>Discord Integration Transcript</h1>;
+  return (
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <h2 className={styles.logo}>Logo</h2>
+        <nav className={styles.navigation}>
+          <a className={styles.home} href="#">
+            Home
+          </a>
+          <a className={styles.home} href="#">
+            Sobre
+          </a>
+          <a className={styles.home} href="#">
+            Dashboard
+          </a>
+          <button className={styles.button}>Entrar com discord</button>
+        </nav>
+      </header>
+      <section className={styles.home2}>
+        <div className={styles.homeContent}>
+          <h1>Hello, i am BOT NAME</h1>
+          <h3>I'm a custom discord bot</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            tristique est sed facilisis ultrices. Vivamus commodo pharetra
+            purus, a lacinia ex scelerisque at. Mauris risus nisl, consectetur
+            quis semper nec, iaculis ac mi. Maecenas ut mauris ornare metus
+            consequat cursus id et mauris.
+          </p>
+          <div className={styles.morebutton}>
+            <a href="https://discordapp.com/users/337012320284835840">
+              Saber mais
+            </a>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
